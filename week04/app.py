eddb40446e5eb0c8e25a4898cb015cc23fabf926
chem2stry <chem2stry2p@gmail.com>
2026-09-15 21:38:27 +0900

    preprocess missing values and outliers

--- week04/app.py
+++
@@ -1,4 +0,0 @@
-import mymath
-number = int(input("Enter a Number: "))
-print(f"{number}! = {mymath.factorial(number)}")
-print(f"{number}! = {mymath.factorial_iter(number)}")
--- week04/solution.py
+++ week04/solution.py
@@ -2,6 +2,6 @@ import numpy as np
 import pandas as pd
 
 df = pd.read_csv('smart_classroom.csv')
-df.rename(columns={'timestamp':'Datetime'})
+df = df.rename(columns={'timestamp':'Datetime'})
 df = df.set_index('Datetime')
 
